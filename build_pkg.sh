864944e6f6c6e7f5869e5a65af4b97790fce6ebf
Szymon Rucinski <szymon.rucinski@fhnw.ch>
2023-02-05 00:21:41 +0100

    ⚡️ add CI

--- build_pkg.sh
+++ build_pkg.sh
@@ -1,3 +1,2 @@
-python setup.py sdist bdist_wheel
-cd dist
-pip install pippi-0.0.1-py3-none-any.whl
+pipenv run python setup.py sdist bdist_wheel
+pipenv run pip ./dist/install pippi-0.0.1-py3-none-any.whl
