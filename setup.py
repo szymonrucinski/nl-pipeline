89cdba0730206dfef8d0a70178608bd41e247a83
szymonrucinski <szymonrucinski@icloud.com>
2023-02-11 00:28:04 +0100

    ⚡️ Update release version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 DESCRIPTION = "A simple package to create elegant nlp pipelines using sklearn."
 
 # Setting up
